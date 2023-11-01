18908c83530808cb5951b3b286dac1f52b142464
Crowdbotics <team@crowdbotics.com>
2023-11-01 11:00:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blood-pressure-moni-44264",
+  url: "https://blood-pressure-moni-44264.botics.co",
+  api: "https://blood-pressure-moni-44264.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
